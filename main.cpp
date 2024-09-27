b531f4da278f4b4165b0adbb6cfcae1d24b32aad
AntPerc <antonio.percuoco@marconibari.edu.it>
2024-09-27 14:09:07 +0000

    ho aggiunto uno spazio

--- main.cpp
+++ main.cpp
@@ -2,10 +2,32 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n, i, conDebito, contPromossi;
+    double voto, somma, media;
+
+    do {
+        cout << "quanti studenti ci sono in classe?" << endl;
+        cin >> n;
+    } while (n < 0);
+    contPromossi = 0;
+    conDebito = 0;
+    somma = 0;
+    i = 0;
+    while (i < n) {
+        cout << "inserisci il voto del" << i + 1 << "alunno" << endl;
+        cin >> voto;
+        if (voto >= 6) {
+            contPromossi = contPromossi + 1;
+        } else {
+            conDebito = conDebito + 1;
+        }
+        i = i + 1;
+        somma = somma + voto;
+    }
+    media = somma / n;
+    cout << "saranno promossi" << contPromossi << "studenti. Avranno il debito" << conDebito << " studenti" << endl;
+    cout << "la media dei voti della classe è" << media << endl;
+    return 0; 
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
